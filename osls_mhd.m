48ee3887999e6c9d1fe6b4d165b5f6cb753bc8f0
Khanh Nguyen <khanh201dth@gmail.com>
2026-04-21 17:48:15 -0500

    add ls mesh solver

--- osls_mhd.m
+++ osls_mhd.m
@@ -4,6 +4,7 @@ set(0, "defaultAxesFontSize", 24)
 set(0, "defaultTextFontSize", 24)
 set(0, "defaultLineLineWidth", 2)
 
+
 N = 250;
 
 Re = 3e4;
@@ -35,28 +36,73 @@ u = umat(:, unstable);
 v = reshape(v, [N+1, 2]);
 u = reshape(u, [N+1, 2]);
 
-eps_list = [];
-err_list = [];
-for k = -16:0
-  eps = 10^(k/2);
-  eps_list(end+1) = eps;
-  [xs,umat,vmat,amat,gamma,f,U] = solve_osls_mhd(alpha, N, eps, rhos, mus, sigmas, st, g, Bx, Bz);
-  % figure;
-  % plot(abs(v), xs', 'linewidth', 2)
-  % title('V'); hold on;
-  % plot(abs(u), xs', 'linewidth', 2);
-  c = gamma*1i/alpha;
-  lambda = alpha*c;
-  [~, unstable] = max(imag(c));
-  c_unstable = c(unstable)
-  err = abs(imag(c_unstable) - imag(c_unstable_2p)) / abs(imag(c_unstable_2p));
-  err_list(end+1) = err;
-  % a = amat(unstable);
-  % v = vmat(:, unstable);
-  % u = umat(:, unstable);
-  % v = reshape(v, [N+1, 2]);
-  % u = reshape(u, [N+1, 2]);
-  % plot(abs(v), xs', 'linewidth', 2)
-  % plot(abs(u), xs', 'linewidth', 2);
-end
-plot(eps_list, err_list)
+% eps_list = [];
+% err_list = [];
+% for k = -16:0
+%   eps = 10^(k/4);
+%   eps_list(end+1) = eps;
+%   [xs,umat,vmat,amat,gamma,f,U] = solve_osls_mhd(alpha, N, eps, rhos, mus, sigmas, st, g, Bx, Bz);
+%   % figure;
+%   % plot(abs(v), xs', 'linewidth', 2)
+%   % title('V'); hold on;
+%   % plot(abs(u), xs', 'linewidth', 2);
+%   c = gamma*1i/alpha;
+%   lambda = alpha*c;
+%   [~, unstable] = max(imag(c));
+%   c_unstable = c(unstable)
+%   err = abs(imag(c_unstable) - imag(c_unstable_2p)) / abs(imag(c_unstable_2p));
+%   err_list(end+1) = err;
+%   % a = amat(unstable);
+%   % v = vmat(:, unstable);
+%   % u = umat(:, unstable);
+%   % v = reshape(v, [N+1, 2]);
+%   % u = reshape(u, [N+1, 2]);
+%   % plot(abs(v), xs', 'linewidth', 2)
+%   % plot(abs(u), xs', 'linewidth', 2);
+% end
+% loglog(eps_list, err_list)
+
+
+% [xs,umat,vmat,amat,gamma,f,U] = solve_osls_mhd(alpha, N, eps, rhos, mus, sigmas, st, g, Bx, Bz);
+% c = gamma*1i/alpha;
+% lambda = alpha*c;
+% [~, unstable] = max(imag(c));
+% c_unstable = c(unstable)
+% err = abs(imag(c_unstable) - imag(c_unstable_2p)) / abs(imag(c_unstable_2p))
+
+
+% Interpolate to Nek mesh
+nely = 40; % Nely in 1 phase; must be divisible by 2 for geometric
+nelx = -60;
+Nf = 8; % lx1
+el_ratio = 16.0000;
+zf = zeros(nely*Nf,1);
+el_pos = linspace(-1, 1, nely + 1); % Linear element
+
+geom_fac = (el_ratio)^(1/(nely/2));
+el_pos_tmp = geom_fac.^(0:nely/2) - 1.0;
+el_pos_tmp = el_pos_tmp/el_pos_tmp(end) - 1.0;
+el_pos_tmp2 = flip(-el_pos_tmp);
+el_pos(1:nely/2+1) = el_pos_tmp;
+el_pos(nely/2+1:end) = el_pos_tmp2;
+
+mesh_z = [0.5*(el_pos-1), 0.5*(el_pos+1)(2:end)];
+
+L = 1;
+el_length = 0.1;
+eps = el_length*1.5/(Nf-1)/L;
+
+[xs,umat,vmat,amat,gamma,f,U] = solve_osls_mhd_mesh(alpha, Nf, mesh_z, eps, rhos, mus, sigmas, st, g, Bx, Bz);
+c = gamma*1i/alpha;
+[~, unstable] = max(imag(c));
+c_unstable = c(unstable)
+err = abs(imag(c_unstable) - imag(c_unstable_2p)) / abs(imag(c_unstable_2p))
+a = amat(unstable);
+v = vmat(:, unstable);
+u = umat(:, unstable);
+v = reshape(v, Nf+1,[]);
+u = reshape(u, Nf+1,[]);
+% figure
+% plot(abs(v), xs', 'linewidth', 2);
+% figure
+% plot(abs(u), xs', 'linewidth', 2);
---
+++ solve_osls_mhd_mesh.m
@@ -0,0 +1,202 @@
+function [xs,umat,vmat,amat,gamma,f,U] = solve_osls_mhd_mesh(alpha, N, mesh_z, eps, rhos, mus, sigmas, st, g, Bx, Bz)
+  [Ah,Bhh,Ch,Dhh,z,w] = semhat(N);
+  [xs,f,Uss] = solve_steady(alpha, N, mesh_z, eps, rhos, mus, sigmas, st, g, Bx, Bz);
+
+  Nelem = length(mesh_z) - 1;
+  nh = N + 1;
+  Ng = Nelem * (N-1) + 2;  % total global nodes
+  Q = sparse([], [], [], Nelem*nh, Ng, 2*Ng);
+  for e = 1:Nelem
+      gidx = (e-1)*(nh-2) + (1:nh);
+      Q((e-1)*nh + (1:nh), gidx) = speye(nh);
+  end
+
+  for e = 1:Nelem
+      z_left = mesh_z(e);
+      z_right = mesh_z(e+1);
+      L = z_right - z_left;
+
+      Dh = 2/L*Dhh;
+      Bh = L/2*Bhh;
+
+      T = speye(nh);
+      T(2, :) = Dh(1,:);
+      T(end-1, :) = speye(nh)(end, :);
+      T(end, :) = Dh(end,:);
+      T = inv(T);
+
+      x = z_left + (z+1.0)/2*L;
+
+      U = Uss(:,e);
+      phi = 0.5*(tanh(x/2/eps)+1);
+      rho = (1-phi)*rhos(1) + phi*rhos(2);
+      mu = (1-phi)*mus(1) + phi*mus(2);
+      sigma = (1-phi)*sigmas(1) + phi*sigmas(2);
+      delta = (1/4/eps)./(cosh(x/2/eps).^2);
+
+      DU = Dh * U;
+      D2U = Dh * DU;
+
+      S = zeros(nh,1);
+      if e == Nelem/2
+          S = zeros(nh,1); S(end,1) = 1;
+      elseif e == (Nelem/2 + 1)
+          S = zeros(nh,1); S(1,1) = -1;
+      end
+      Kuu0 = -Dh'*Dh'*Bh*diag(mu)*Dh*Dh...
+             -4*alpha^2*Dh'*Bh*diag(mu)*Dh...
+             -alpha^2*Bh*diag(mu)*Dh*Dh...
+             -alpha^2*Dh'*Dh'*Bh*diag(mu)...
+             -alpha^4*Bh*diag(mu);
+
+      KuuU = 1i*alpha*Dh'*Bh*diag(rho)*diag(DU)...
+             -1i*alpha*Dh'*Bh*diag(rho)*diag(U)*Dh...
+             -1i*alpha^3*Bh*diag(rho)*diag(U);
+
+      KuuB = -Bx^2*alpha^2*Bh*diag(sigma)... 
+             + Bx*Bz*1i*alpha*Bh*diag(sigma)*Dh...
+             - Bx*Bz*1i*alpha*Dh'*Bh*diag(sigma)...
+             - Bz^2*Dh'*Bh*diag(sigma)*Dh;
+      Kuu = Kuu0 + KuuU + KuuB;
+
+      Kua = -alpha^4*st*Bh*delta...
+            -1i*alpha*(rhos(1)-rhos(2))*f*Dh'*Bh*delta...
+            -alpha^2*(rhos(1)-rhos(2))*g*Bh*delta...
+            +1i*(mus(1)-mus(2))*(Dh'*Dh'*Bh*diag(delta)*DU...
+                 + alpha^2*Bh*diag(delta)*DU)...
+            +1i*alpha*(sigmas(1)-sigmas(2))*Bz^2*(Dh'*Bh*diag(delta)*U)...
+            +alpha^2*(sigmas(1)-sigmas(2))*Bx*Bz*Bh*diag(delta)*U;
+
+      Kau = 0.5*abs(S');  % 0.5 to counter multiplicity
+      Muu = Dh'*Bh*diag(rho)*Dh + alpha^2*Bh*diag(rho);
+
+      Kuu_block((e-1)*(N+1)+1:e*(N+1), (e-1)*(N+1)+1:e*(N+1)) = T'*Kuu*T;
+      Muu_block((e-1)*(N+1)+1:e*(N+1), (e-1)*(N+1)+1:e*(N+1)) = T'*Muu*T;
+      Kau_block(1,(e-1)*(N+1)+1:e*(N+1)) =  Kau*T;
+      Kua_block((e-1)*(N+1)+1:e*(N+1),1) =  T'*Kua;
+      T_block((e-1)*(N+1)+1:e*(N+1), (e-1)*(N+1)+1:e*(N+1)) = T;
+      D_block((e-1)*(N+1)+1:e*(N+1), (e-1)*(N+1)+1:e*(N+1)) = Dh;
+  end
+
+  Ih = speye(Ng); 
+  R=Ih(3:end-2,:);
+
+  Kuu_global = R*Q'*Kuu_block*Q*R';
+  Kua_global = R*Q'*Kua_block;
+  Kau_global = Kau_block*Q*R';
+  Muu_global = R*Q'*Muu_block*Q*R';
+
+  K = [
+    Kuu_global, Kua_global;
+    Kau_global, -1i*alpha*Uss(end,Nelem/2);
+  ];
+  M = [
+    Muu_global, R*Q'*zeros(Nelem*nh, 1);
+    zeros(1, Nelem*nh)*Q*R', 1;
+  ];
+  [vecs, gamma] = eig(K, M, 'vector');
+  for k = 1:size(vecs,2)
+    vecs(:,k) = vecs(:,k) / abs(vecs(end,k));
+  end
+  amat = vecs(end, :);
+  vmat = T_block*Q*R'*vecs(1:end-1, :);
+  umat = 1i/alpha*D_block*vmat;
+
+  U1 = Uss(:,1);
+  U2 = Uss(:,2);
+  % test_error(alpha, N, rhos, mus, sigmas, st, g, umat, vmat, amat, gamma, Bx, Bz, U1, U2, Dh);
+end
+
+function [xs,f,Uss] = solve_steady(alpha, N, mesh_z, eps, rhos, mus, sigmas, st, g, Bx, Bz)
+  [Ah,Bhh,Ch,Dhh,z,w] = semhat(N);
+
+  k1 = sqrt(sigmas(1)/mus(1))*Bz;
+  k2 = sqrt(sigmas(2)/mus(2))*Bz;
+  rat = sqrt(sigmas(1)*mus(1)/sigmas(2)/mus(2));
+  f = Bz^2/(...
+      rhos(1)/sigmas(1)...
+      -(rhos(2)/sigmas(2) + rat*rhos(1)/sigmas(1)/sinh(k1)*sinh(k2) + (rhos(1)/sigmas(1) - rhos(2)/sigmas(2))*cosh(k2))...
+      /(cosh(k2) + rat*coth(k1)*sinh(k2))...
+  );
+
+  Nelem = length(mesh_z) - 1;
+  nh = N + 1;
+  Ng = Nelem * N + 1;  % total global nodes
+  Q = sparse([], [], [], Nelem*nh, Ng, 2*Ng);
+  for e = 1:Nelem
+      gidx = (e-1)*(nh-1) + (1:nh);
+      Q((e-1)*nh + (1:nh), gidx) = speye(nh);
+  end
+  xs = [];
+  rho_arr = [];
+  for e = 1:Nelem
+      z_left = mesh_z(e);
+      z_right = mesh_z(e+1);
+      L = z_right - z_left;
+
+      Dh = 2/L*Dhh;
+      Bh = L/2*Bhh;
+
+      x = z_left + (z+1.0)/2*L;
+      xs = [xs; x'];
+      phi = 0.5*(tanh(x/2/eps)+1);
+      rho = (1-phi)*rhos(1) + phi*rhos(2);
+      mu = (1-phi)*mus(1) + phi*mus(2);
+      sigma = (1-phi)*sigmas(1) + phi*sigmas(2);
+      rho_arr = [rho_arr; rho];
+
+      K = Bh*diag(Dh*mu)*Dh + Bh*diag(mu)*Dh*Dh  - Bz^2*Bh*diag(sigma);
+      K = -Dh'*Bh*diag(mu)*Dh  - Bz^2*Bh*diag(sigma);
+
+      Kuu_block((e-1)*(N+1)+1:e*(N+1), (e-1)*(N+1)+1:e*(N+1)) = K;
+      rhs((e-1)*(N+1)+1:e*(N+1)) = -Bh*f*rho;
+  end
+
+  Ih = speye(Ng); 
+  R=Ih(2:end-1,:);
+  Kuu_global = R*Q'*Kuu_block*Q*R';
+  rhs = R*Q'*rhs';
+
+  U = Q*R'*(Kuu_global \ rhs);
+  Uss = reshape(U, [nh, Nelem]);
+  % figure
+  % plot(xs',Uss);
+  % plot(xs',rho_arr);
+end
+
+function test_error(alpha, N, rhos, mus, sigmas, st, g, umat, vmat, amat, gamma_temp, Bx, Bz, U1, U2, Dh)
+  [~, idx] = sort(real(gamma_temp), 'descend');
+  unstable = idx(1:min(4, length(idx)));
+  v1 = vmat(1:N+1,unstable);
+  v2 = vmat(N+2:end,unstable);
+  DU1 = Dh*U1;
+  DU2 = Dh*U2;
+  D2U1 = Dh*DU1;
+  D2U2 = Dh*DU2;
+  D2v1 = Dh*Dh*v1;
+  D2v2 = Dh*Dh*v2;
+  Dv1 = Dh*v1;
+  Dv2 = Dh*v2;
+  D3v1 = Dh*Dh*Dv1;
+  D3v2 = Dh*Dh*Dv2;
+  a = amat(unstable);
+  gamma = gamma_temp(unstable).';
+
+  shear_stress_error = (mus(1)*D2U1(end)-mus(2)*D2U2(1))*a...
+                       + mus(1)*1i*alpha*v1(end, :) - mus(2)*1i*alpha*v2(1, :)...
+                       - mus(1)/1i/alpha*D2v1(end, :) + mus(2)/1i/alpha*D2v2(1, :)
+  normal_stress_error = (rhos(1)-rhos(2))*g*a...
+                       + rhos(1)*(gamma/alpha^2 - U1(end)/1i/alpha).*Dv1(end, :)...
+                       - rhos(2)*(gamma/alpha^2 - U2(1)/1i/alpha).*Dv2(1, :)...
+                       - mus(1)/alpha^2*D3v1(end, :) + mus(2)/alpha^2*D3v2(1, :)...
+                       + 3*mus(1)*Dv1(end, :) - 3*mus(2)*Dv2(1, :)...
+                       + rhos(1)*DU1(end)/1i/alpha*v1(end,:)...
+                       - rhos(2)*DU2(1)/1i/alpha*v2(1,:)...
+                       + sigmas(1)/alpha^2*Bz^2*Dv1(end,:)...
+                       - sigmas(2)/alpha^2*Bz^2*Dv2(1,:)...
+                       - sigmas(1)/1i/alpha*Bz*Bx*v1(end,:)...
+                       - sigmas(2)/1i/alpha*Bz*Bx*v2(1,:)...
+                       + st*alpha^2*a
+  kinematic_error = v1(end, :) - (gamma + 1i*alpha*U1(end)).*a
+
+end
